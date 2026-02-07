74cd27e8a83666fc447366f3c9739176f3618566
mckayhyorgason <mckay.yorgason21@gmail.com>
2026-02-07 11:53:31 -0700

    Services page

--- MyHobby/wwwroot/js/site.js
+++ MyHobby/wwwroot/js/site.js
@@ -2,3 +2,16 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+$("#calc").click(function () {
+    let hours = parseInt($("#hours").val())
+    
+    if (isNaN(hours) || hours <= 0 ) {
+        alert("Please enter a positive number of hours.");
+        return;
+    }
+    
+    let rate = 50;
+    let total = hours * rate;
+    $("#total").text("$" + total);
+})
